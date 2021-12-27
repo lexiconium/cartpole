1092ac1ca8800c7585c8904241418ac0028b5f7d
Minsoo Kim <min-soo.kim@outlook.kr>
2021-12-27 23:54:58 +0900

    random seed fixer

--- utils/seed.py
+++ utils/seed.py
@@ -1,10 +1,20 @@
+import os
 import random
 
 import torch
+import torch.backends.cudnn as cudnn
+
 import numpy as np
 
 
 def seed(seed: int = 42):
+    os.environ["PYTHONHASHSEED"] = str(seed)
     random.seed(seed)
+
     torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    cudnn.benchmark = False
+    cudnn.deterministic = True
+
     np.random.seed(seed)
