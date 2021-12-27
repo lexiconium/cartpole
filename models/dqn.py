23e39d980513a045aa29b6223d4a827fc086907c
Minsoo Kim <min-soo.kim@outlook.kr>
2021-12-27 22:47:52 +0900

    dqn model

--- models/dqn.py
+++ models/dqn.py
@@ -1,24 +1,32 @@
 import random
+from typing import List
 
 import torch
 from torch import nn
 
-from .core import mlp
+from .core.core import mlp
 
 
 class DQN(nn.Module):
-    def __init__(self, obs_dim, hidden_dims, act_dim, activation):
+    def __init__(
+        self,
+        observation_dim: int,
+        hidden_dims: List[int],
+        action_dim: int,
+        activation: nn.Module,
+        p_dropout: float,
+    ):
         super().__init__()
-        self.act_dim = act_dim
-        layer_dims = [obs_dim, *hidden_dims, act_dim]
-        self.layers = mlp(layer_dims=layer_dims, activation=activation, p_dropout=0.1)
+        self.action_dim = action_dim
+        layer_dims = [observation_dim, *hidden_dims, action_dim]
+        self.layers = mlp(layer_dims=layer_dims, activation=activation, p_dropout=p_dropout)
 
-    def forward(self, obs):
-        return self.layers(obs)
+    def forward(self, observations: torch.Tensor):
+        return self.layers(observations)
 
     @torch.no_grad()
-    def action(self, obs, eps):
-        if random.random() < eps:
-            return torch.tensor(random.randrange(self.act_dim))
+    def get_action(self, observation: torch.Tensor, epsilon: float):
+        if random.random() < epsilon:
+            return random.randrange(self.action_dim)
         else:
-            return self.forward(obs).argmax()
+            return self(observation).argmax().item()
