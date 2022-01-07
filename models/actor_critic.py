dfa0d6b310b6852ecbf043110768e2875f3fe939
Minsoo Kim <min-soo.kim@outlook.kr>
2022-01-07 21:19:39 +0900

    parameter sharing actor-critic

--- models/actor_critic.py
+++ models/actor_critic.py
@@ -38,3 +38,35 @@ class Critic(nn.Module):
 
     def forward(self, observations: torch.Tensor):
         return self.layers(observations)
+
+
+class ActorCritic(nn.Module):
+    def __init__(
+        self,
+        observation_dim: int,
+        hidden_dims: List[int],
+        action_dim: int,
+        activation: nn.Module,
+        p_dropout: float,
+    ):
+        super().__init__()
+        common_layer_dims = [observation_dim, *hidden_dims]
+        actor_layer_dims = [hidden_dims[-1], action_dim]
+        critic_layer_dims = [hidden_dims[-1], 1]
+
+        self.common_layers = mlp(
+            layer_dims=common_layer_dims,
+            activation=activation,
+            output_activation=activation,
+            p_dropout=p_dropout,
+        )
+        self.actor_layer = mlp(layer_dims=actor_layer_dims, p_dropout=p_dropout)
+        self.critic_layer = mlp(layer_dims=critic_layer_dims, p_dropout=p_dropout)
+
+    def actor(self, observations: torch.Tensor):
+        x = self.common_layers(observations)
+        return self.actor_layer(x)
+
+    def critic(self, observations: torch.Tensor):
+        x = self.common_layers(observations)
+        return self.critic_layer(x)
