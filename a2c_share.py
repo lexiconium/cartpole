b1843722b6c9dca3bda73356ec59479e731642bf
Minsoo Kim <min-soo.kim@outlook.kr>
2022-01-11 19:02:48 +0900

    advantage actor-critic w/ param share

---
+++ a2c_share.py
@@ -0,0 +1,119 @@
+import argparse
+import logging
+import multiprocessing
+from itertools import count
+
+import torch
+from torch import nn, optim
+import torch.nn.functional as F
+import numpy as np
+import gym
+
+from models.actor_critic import Actor, ActorCritic
+from utils.parallelism import ParallelEnvs
+from utils.logger import SimpleLoggerWrapper, ScoreLogger
+
+TRAINING_STEPS = 100000
+
+
+def compute_bellman(state_values, reward_records, done_records, gamma):
+    targets = [
+        (state_values := rewards + masks * gamma * state_values)
+        for rewards, masks in zip(reward_records[::-1], done_records[::-1])
+    ][::-1]
+    return torch.from_numpy(np.array(targets)).float()
+
+
+def test(id: str, actor: Actor):
+    env = gym.make(id)
+
+    durations = []
+    for _ in range(10):
+        state = env.reset()
+        for t in count(1):
+            output = actor(torch.tensor(state).float())
+            action = output.argmax(dim=-1).item()
+            next_state, reward, done, _ = env.step(action)
+
+            if done:
+                durations.append(t)
+                break
+            state = next_state
+
+    env.close()
+    print(f"Average Duration: {np.array(durations).mean(axis=-1)}")
+
+
+def train(args: argparse.Namespace):
+    envs = ParallelEnvs(id=args.env, num_parallel=multiprocessing.cpu_count())
+    config = {
+        "observation_dim": envs.observation_space.shape[0],
+        "action_dim": envs.action_space.n,
+        "hidden_dims": [128],
+        "activation": nn.GELU,
+        "p_dropout": 0,
+    }
+    actor_critic = ActorCritic(**config)
+    optimizer = optim.AdamW(
+        params=actor_critic.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay
+    )
+
+    states = envs.reset()
+    step = 0
+    while step < TRAINING_STEPS:
+        state_records, action_records, reward_records, done_records = [], [], [], []
+        for _ in range(args.update_interval):
+            probabilities = F.softmax(actor_critic.actor(torch.tensor(states).float()), dim=-1)
+            actions = torch.multinomial(probabilities, num_samples=1).view(-1).numpy()
+            next_states, rewards, dones = envs.step(actions)
+
+            state_records.append(states)
+            action_records.append(actions)
+            reward_records.append(rewards / 100)
+            done_records.append(1 - dones)
+
+            states = next_states
+
+        step += args.update_interval
+
+        final_state_values = actor_critic.critic(torch.tensor(states).float()).view(-1).detach().numpy()
+        targets = compute_bellman(final_state_values, reward_records, done_records, args.gamma)
+
+        state_records = torch.from_numpy(np.array(state_records)).float()
+        state_values = actor_critic.critic(state_records).squeeze(dim=-1)
+
+        advantages = targets - state_values
+
+        action_records = torch.from_numpy(np.array(action_records)).unsqueeze(dim=-1)
+        negative_log = (
+            -torch.log_softmax(actor_critic.actor(state_records), dim=-1)
+            .gather(dim=-1, index=action_records)
+            .squeeze(dim=-1)
+        )
+        loss = (negative_log * advantages).mean() + F.smooth_l1_loss(state_values, targets)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if step % (args.update_interval * 10) == 0:
+            test(id=args.env, actor=actor_critic.actor)
+
+    envs.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--env", type=str, default="CartPole-v1")
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--num_episodes", type=int, default=10000)
+    parser.add_argument("--update_interval", type=int, default=20)
+    parser.add_argument("--learning_rate", type=float, default=5e-4)
+    parser.add_argument("--weight_decay", type=float, default=1e-3)
+    parser.add_argument("--gamma", type=float, default=0.98)
+    parser.add_argument("--epsilon", type=float, default=0.08)
+    parser.add_argument("--seed", type=int, default=42)
+
+    args = parser.parse_args()
+
+    train(args)
